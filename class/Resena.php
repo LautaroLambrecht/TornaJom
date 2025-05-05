88467fec68fad1b49c489ea749ec75c8c7955359
Lautaro <lalaxx@alumnatflorida.es>
2025-05-05 10:28:43 +0200

    Finalizado clases Resena

---
+++ class/Resena.php
@@ -0,0 +1,35 @@
+<?php
+
+    class Resena{
+
+        private $id;
+        private $estrellas;
+        private $descripcion;
+        private $id_usuario;
+        private $tipo;
+
+        public function __construct($estrellas, $descripcion, $id_usuario, $tipo){
+            $this->estrellas = $estrellas;
+            $this->descripcion = $descripcion;
+            $this->id_usuario = $id_usuario;
+            $this->tipo = $tipo;
+        }
+
+        public function getId(){
+            return $this->id;
+        }
+
+        public function getEstrellas(){
+            return $this->estrellas;
+        }
+
+        public function getIdUsuario(){
+            return $this->id_usuario;
+        }
+
+        public function getTipo(){
+            return $this->tipo;
+        }
+    }
+
+?>
