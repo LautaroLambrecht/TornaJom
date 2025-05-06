8d329586d51526de0a3ce81dd5c37d965fdb9489
saalxx <saalxx@alumnatflorida.es>
2025-05-06 18:54:04 +0200

    terminado el modificar de trabajos

--- class/Model.php
+++ class/Model.php
@@ -18,10 +18,21 @@
                     if ($contador % 2 == 0){
                         echo "<div style='display:flex; flex-direction:row; gap:50px; justify-content:center'>";
                     }
-                    echo "<div class='trabajo' style='width:40%; height:320px';>
+                    echo "<div class='trabajo' style='width:40%; height:340px';>
                         <img src='img\Electricista.jpg' style='justify-content:center; width:100%;'>
                         <h1 style='font-size:15px;width:100%;'>".$row['titulo']."</h1>
                         <p>".$row['descripcion']."</p>
+                        <form action='update.php' method='POST'>
+                        <input type='hidden' name='id' value='".$row['id']."'> 
+                        <input type='hidden' name='titulo' value='".$row['titulo']."'>
+                        <input type='hidden' name='descripcion' value='".$row['descripcion']."'>
+                        <input type='hidden' name='estado' value='".$row['estado']."'>
+                        <input type='hidden' name='id_publicacion' value='".$row['id_publicacion']."'>
+                        <input type='hidden' name='id_realizacion' value='".$row['id_realizacion']."'>
+                        <input type='hidden' name='zona' value='".$row['zona']."'>
+                        <input type='hidden' name='id_especialidad' value='".$row['id_especialidad']."'>
+                        <button type='submit' style='background-color: #74ACDF; color: white; font-weight: 600; border: none; transition: background-color 0.3s ease; cursor: pointer; font-weight: 60; font-size: 1rem;border-radius: 0.5rem;padding: 0.45rem 1.5rem;width: 160px; height:30px'>MODIFICAR</button>
+                        </form>
                     </div>";
                     $contador++;
                     if ($contador % 2 == 0){
@@ -35,10 +46,10 @@
         }
         
 
-        public function updateWorks($id, $estado, $titulo, $descripcion, $id_publicacion, $id_realizacion, $zona, $especialidad){
-            $sql= "update trabajo set titulo='$titulo', estado='$estado', descr$descripcion='$descripcion', id_publicacion = '$id_publicacion', id_realizacion='$id_realizacion', zona='$zona', especialidad = '$especialidad' where id='$id'";
+        public function updateWorks($id, $estado, $titulo, $descripcion, $zona, $id_especialidad){
+            $sql= "update trabajo set titulo='$titulo', estado='$estado', descripcion='$descripcion', zona='$zona', id_especialidad = '$id_especialidad' where id='$id'";
             $this->conn->query($sql);
-            return $updatWoks;
+            
         }
     }
 
