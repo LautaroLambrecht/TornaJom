24a537428b0e1a4da516af78ea20e34468ad3e9d
Lautaro <lalaxx@alumnatflorida.es>
2025-05-06 08:57:02 +0200

    comienzo de index

--- class/Model.php
+++ class/Model.php
@@ -5,7 +5,7 @@
     class Model extends Conection{
 
         public function getAllWorks(){
-            $sql = "SELECT * FROM trabajo where estado <> 'completado'";
+            $sql = "SELECT * FROM trabajo";
             $result = $this->conn->query($sql);
             return $result;
         }
@@ -13,13 +13,15 @@
         public function drawAllWorks(){
             $result = $this->getAllWorks();
             if ($result->rowCount() > 0){
+                echo "<div style='display:flex; flex-direction:row; gap:50px; justify-content:center'>";
                 while ($row = $result->fetch(PDO::FETCH_ASSOC)){
-                    echo "<div style='width:50%; height:320px'; display:flex; flex-direction:row;>
-                        <img src='img\Electricista.jpg' style='justify-content:center'>
-                        <h1 style='font-size:15px;width:135px;'>".$row['titulo']."</h1>
+                    echo "<div style='width:40%; height:320px';>
+                        <img src='img\Electricista.jpg' style='justify-content:center; width:100%;'>
+                        <h1 style='font-size:15px;width:100%;'>".$row['titulo']."</h1>
                         <p style=''>".$row['descripcion']."</p>
                     </div>";
                 }
+                echo "</div>";
             }
         }
         
